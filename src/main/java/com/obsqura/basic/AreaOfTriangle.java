2103c5c78cabdce59867d684e434931187146439
Githi39 <githi39@gmail.com>
2021-12-09 21:56:34 +0530

    triangle class

---
+++ src/main/java/com/obsqura/basic/AreaOfTriangle.java
@@ -0,0 +1,19 @@
+package com.obsqura.basic;
+
+public class AreaOfTriangle {
+    public static void main(String args[])
+    {
+
+
+        double b= 50;
+        System.out.println("width of the Triangle:"+b);
+
+        double h= 100;
+        System.out.println("height of the Triangle:"+h);
+
+
+
+        double area=(b*h)/2;
+        System.out.println("Area of Triangle is: " + area);
+    }
+}
