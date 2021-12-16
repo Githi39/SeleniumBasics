1d3d2aca260032feba577777f9f063acfce68e84
Githi39 <githi39@gmail.com>
2021-12-16 22:07:14 +0530

    Welcome: Mercury Tours

---
+++ src/main/java/com/obsqura/seleniumpractise/SeleniumcodePractise.java
@@ -0,0 +1,21 @@
+package com.obsqura.seleniumpractise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumcodePractise {
+    public static void main(String args[])
+    {
+        System.setProperty("webdriver.chrome.driver","D:\\selenium\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("http://demo.guru99.com/test/newtours/");
+        //driver.close();
+        String actulTitle = driver.getTitle();
+        System.out.println(actulTitle);
+        String PageSource = driver.getPageSource();
+       System.out.println(PageSource);
+
+
+    }
+}
